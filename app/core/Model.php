dbf6fc5999097a4586152fd4cc598dcc2b31897e
Imon <contact@imonweb.co.uk>
2024-06-02 23:27:41 +0100

    model

--- app/core/Model.php
+++ app/core/Model.php
@@ -9,6 +9,8 @@
 class Model 
 {
   use Database;
+  protected $table = 'users';
+
   // function test()
   // {
   //   $query = "select * from users ";
@@ -16,17 +18,37 @@ class Model
   //   show($result);
   // }
 
-  public function insert()
+  public function where($data, $data_not)
+  {
+    $keys = array_keys($data);
+    $keys_not = array_keys($data_not);
+    $query = "";
+
+    foreach ($keys as $key){
+      $query .= $key . "= :" . $key . " && ";
+    }
+
+    foreach ($keys_not as $key){
+      $query .= $key . "!= :" . $key . " && ";
+    }
+
+    $query = trim($queryt, " && ");
+
+    $query = "select * from $this->table where id = :id && data = now() && id != :id "
+    $this->query($query, ['id' => 23]); 
+  }
+
+  public function insert($data)
   {
 
   }
 
-  public function update()
+  public function update($id, $data, $id_column = 'id')
   {
 
   }
 
-  public function delete()
+  public function delete($id)
   {
 
   }
